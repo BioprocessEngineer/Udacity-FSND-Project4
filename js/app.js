d5b774e70d2c09666a1c12239142e7715627816b
BioprocessEngineer <mattxj@gmail.com>
2017-08-17 09:54:38 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var ViewModel = function() {
         self.courtList.push(new court(courtItem));});
 
     //Intialize variables for city filtering function
-    self.availableCities = ko.observableArray(['All', 'Kitchener', 'Cambridge']);
+    self.availableCities = ko.observableArray(['All', 'GTA', 'Outside-GTA'])
     self.selectedCity = ko.observable("All");
 
 
@@ -15,18 +15,17 @@ var ViewModel = function() {
     self.listClick = function(filteredCourts){
         var ci = filteredCourts.ID;
         google.maps.event.trigger(markers[ci],'click');
-    };
+    }
 
     self.listMouseover = function(filteredCourts){
         var ci = filteredCourts.ID;
         google.maps.event.trigger(markers[ci],'mouseover');
-    };
+    }
 
     self.listMouseout = function(filteredCourts){
         var ci = filteredCourts.ID;
         google.maps.event.trigger(markers[ci],'mouseout');
-    };
-
+    }
 
     //Filter cities where courts are located
     self.filteredCourts = ko.computed(function(){
@@ -34,23 +33,21 @@ var ViewModel = function() {
 
         if (city === "All"){
         filteredLocations = locations;
+        filterMarker();
         return self.courtList();
         }
-
         else {
         var tempList = self.courtList.slice();
         var tempLocations = locations.slice();
-
         filteredLocations = [];
         filteredLocations = tempLocations.filter(function(crt){
             return crt.city === city;
             });
-
+        filterMarker();
         return tempList.filter(function(crt){
             return crt.city === city;
             });
-
-        }
+        };
     });
 };
 
@@ -59,31 +56,22 @@ var court = function(data) {
     this.title = data.title,
     this.location = data.location,
     this.city = data.city,
-    this.ID = data.ID;
+    this.ID = data.ID
 };
 
 //Global objects
 var map;
 var markers = [];
 var city;
-
-var locations = [
-{title: 'Admiral Park Basketball Court', location: {lat: 43.437292, lng: -80.507840}, city: 'Kitchener', ID: '0'},
-{title: 'Caryndale Park Basketball Court', location: {lat: 43.384687, lng: -80.451839}, city: 'Kitchener', ID: '1'},
-{title: 'Doon Pioneer Park C.C. Basketball Court', location: {lat: 43.393900, lng: -80.436895}, city: 'Kitchener', ID: '2'},
-{title: 'Guelph Park Basketball Court', location: {lat: 43.466636, lng: -80.483150}, city: 'Kitchener', ID: '3'},
-{title: 'Weber Park Basketball Court', location: {lat: 43.458268, lng: -80.474419}, city: 'Kitchener', ID: '4'},
-{title: 'Tremaine Park Basketball Court', location: {lat: 43.442125, lng: -80.410050}, city: 'Kitchener', ID: '5'},
-{title: 'Brent Park Basketball Court', location: {lat: 43.393868, lng: -80.339230}, city: 'Cambridge', ID: '6'},
-{title: 'Chirchill Park Basketball Court', location: {lat: 43.342612, lng: -80.303474}, city: 'Cambridge', ID: '7'},
-{title: 'Gordon Chaplin Park Basketball Court', location: {lat: 43.374321, lng: -80.310987}, city: 'Cambridge', ID: '8'},
-{title: 'Santa Maria Park Basketball Court', location: {lat: 43.347730, lng: -80.339230}, city: 'Cambridge', ID: '9'},
-{title: 'Brent Park Basketball Court', location: {lat: 43.393868, lng: -80.282874}, city: 'Cambridge', ID: '10'}
-];
-
 var filteredLocations = [];
-
-
+var locations = [
+{title: 'Admiral Park Basketball Court', location: {lat: 43.437292, lng: -80.507840}, city: 'GTA', ID: '0'},
+{title: 'Ambleside Park Basketball Court', location: {lat: 43.0154219, lng: -81.2940176}, city: 'Outside-GTA', ID: '1'},
+{title: 'Alton Parker Park Basketball Court', location: {lat: 42.312723, lng: -83.028019}, city: 'Outside-GTA', ID: '2'},
+{title: 'Brock Park Basketball Court', location: {lat: 44.381632, lng: -79.696755}, city: 'Outside-GTA', ID: '3'},
+{title: 'David Crombie Park Basketball Court', location: {lat: 43.648182, lng: -79.370273}, city: 'GTA', ID: '4'},
+{title: 'Bennetto Park Basketball Court', location: {lat: 43.269776, lng: -79.861550}, city: 'Outside-GTA', ID: '5'},
+];
 
 function initMap() {
     //Initialise map
@@ -98,48 +86,37 @@ function initMap() {
     //Import location data into markers array and intialize markers
 
     function showMarker(markerLocation){
-    for (var i = 0; i < filteredLocations.length; i++) {
-        var position = filteredLocations[i].location;
-        var title = filteredLocations[i].title;
-        var city = filteredLocations[i].city;
-        // let num = i;
-        var marker = new google.maps.Marker({
-            map: map,
-            icon: 'img/bball_s.png',
-            position: position,
-            title: title,
-            animation: '',
-            id: i,
-            city: city,
-        });
+        for (var i = 0; i < filteredLocations.length; i++) {
+            var position = filteredLocations[i].location;
+            var title = filteredLocations[i].title;
+            var city = filteredLocations[i].city;
+            // let num = i;
+            var marker = new google.maps.Marker({
+                map: map,
+                icon: 'img/bball_s.png',
+                position: position,
+                title: title,
+                animation: '',
+                id: i,
+                city: city,
+            });
+            markers.push(marker);
+            bounds.extend(marker.position);
 
-        markers.push(marker);
-        bounds.extend(marker.position);
-
-        //Add event listeners to markers
-        marker.addListener('click', mclick);
-        marker.addListener('mouseover', mover);
-        marker.addListener('mouseout', mout);
+            //Add event listeners to markers
+            marker.addListener('click', mclick);
+            marker.addListener('mouseover', mover);
+            marker.addListener('mouseout', mout);
         }
         map.fitBounds(bounds);
-    }
+    };
     showMarker(filteredLocations);
+    google.maps.event.addDomListener(document.getElementById('city'), 'change', windowClose);
 
-    google.maps.event.addDomListener(document.getElementById('city'), 'change', filterMarker);
-
-    function filterMarker () {
+    function windowClose () {
         infowindow.close();
-        hideListings();
-        for (var i = 0; i < markers.length; i++) {
-            if (markers[i].city === city) {
-                markers[i].setVisible(true);
-            } else if (city === "All") {
-                markers[i].setVisible(true);
-            }   
-        }
     }
-
-   
+  
     function mover () {
         var marker = this;
         this.setIcon('img/bball_sf.png');
@@ -150,43 +127,58 @@ function initMap() {
         if (infowindow.marker != this) {
         this.setIcon('img/bball_s.png');
             }
-
     }
-
  
     // Initialize info window and retrieve weather info for each court locaton using OpenWeatherMap API
     function mclick() {
         var marker = this;
         if (infowindow.marker != marker) {                
             infowindow.marker = marker;
+            infowindow.setContent(null);
             this.setIcon('img/bball_sf.png');
             for (var i = 0; i < markers.length; i++) {
                 if (markers[i] != this) {
                 markers[i].setIcon('img/bball_s.png');}
                 }
-                
-            infowindow.setContent('<div id="mtitle">' + marker.title + '</div>' + '<div id = "wcourt">Current Condition: </div>');
+            var wurl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + marker.position.lat() + '&lon=' + marker.position.lng() + '&APPID=5b37904a067f4ea5c74a6b365b07dbc4';
+            $.getJSON(wurl, function(data) {
+            var wcourt = data.weather[0].description;
+            infowindow.setContent('<div id="mtitle">' + marker.title + '</div>' + '<div id = "wcourt">Current Condition: ' + wcourt + '</div>');
+            })
+            .fail(function(){
+                alert("Error with OpenWeatherMap API");
+            })                
             infowindow.open(map, marker);
             infowindow.addListener('closeclick', function(){
                 infowindow.setContent(null);
             });
-            var wurl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + marker.position.lat() + '&lon=' + marker.position.lng() + '&APPID=5b37904a067f4ea5c74a6b365b07dbc4';
-            $.getJSON(wurl, function(data) {
-            var wcourt;
-            wcourt = data.weather[0].description;
-            document.getElementById('wcourt').append(wcourt);
-        });
-    }}
+    }};
+}
 
 
-   //Function to hide markers
-    function hideListings () {
-        for (var i = 0; i < markers.length; i++) {
-            markers[i].setVisible(false);
-            markers[i].setIcon('img/bball_s.png');
-        }
+//Function to filter markers
+function filterMarker () {
+    hideListings();
+    for (var i = 0; i < markers.length; i++) {
+        if (markers[i].city === city) {
+            markers[i].setVisible(true)
+        } else if (city === "All") {
+            markers[i].setVisible(true)
+        }   
     }
+}
 
+//Function to hide markers
+function hideListings () {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setVisible(false);
+        markers[i].setIcon('img/bball_s.png')
+    }
+}
+
+//Function to handle errors when loading googleMAP API
+function mapAPI () {
+    alert("Error with GoogleMap API");
 }
 
 ko.applyBindings(new ViewModel());
